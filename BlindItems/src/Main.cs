ec9c4c69ae0cdcded871d2789eed5a419df0ee4d
MarkTullius <mark_standerwick@live.com>
2024-01-19 11:10:10 +0000

    first commit

---
+++ BlindItems/src/Main.cs
@@ -0,0 +1,19 @@
+using BepInEx;
+using RoR2;
+
+namespace ModTemplate
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+
+    public class Main : BaseUnityPlugin
+    {
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "MarkTullius";
+        public const string PluginName = "BlindItems";
+        public const string PluginVersion = "0.0.1";
+
+        public void Awake()
+        {
+        }
+    }
+}
